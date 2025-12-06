5e59800ad4d8468bcd84ac2d46567abc5f00d0c6
Halffd <guhalfeld@hotmail.com>
2025-12-05 23:06:00 -0300

    Fri Dec  5 11:06:00 PM -03 2025

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import App from '../../sandbox/ts/my-app/components/App';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
